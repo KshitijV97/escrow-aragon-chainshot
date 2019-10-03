d3bd61d55e07fb909efb470e86ba9cf124fe128c
KshitijV97 <writetokshitij@gmail.com>
2019-10-03 21:45:50 +0530

    Create 5_Require.sol

---
+++ 5_Require.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.4.19;
+
+contract EscrowContract {
+	address public depositor;
+	address public beneficiary;
+	address public arbiter;
+
+	function EscrowContract(address _arbiter, address _beneficiary)
+		public payable
+	{
+		arbiter = _arbiter;
+		beneficiary = _beneficiary;
+		depositor = msg.sender;
+	}
+
+	function approve() public
+	{
+		require( msg.sender == arbiter);
+		beneficiary.transfer(this.balance);
+	}
+}
